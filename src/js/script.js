58d638cb90314153c84b398c36423fc0bd0710a6
humberto <humber.arraya@gmail.com>
2025-01-24 13:44:43 -0300

    style: update notification classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
 const buttonMarkAll = document.getElementById('markAll');
 const listNotification = document.querySelectorAll('li');
 const numbersOfNotifications = document.getElementById('countNotification');
+const textUnread = document.querySelectorAll('.unread')
 
-// when clicked deletes all unread classes
+// when clicked deletes all "unread" classes
 
 buttonMarkAll.addEventListener('click', () => {
   listNotification.forEach(list => {
-    list.classList.remove('unread')
+    list.classList.remove('active')
   });
+
+  textUnread.forEach(item => {
+    item.classList.remove('unread')
+  })
+
   numbersOfNotifications.innerHTML = 0;
 })
